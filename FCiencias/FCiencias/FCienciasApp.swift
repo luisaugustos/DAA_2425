e6e3ca01191abd5cb03c9be79f95e84cfa480729
LUIS <luisaugustos@usal.es>
2024-10-04 09:34:28 +0200

    fixed app Fciencias

---
+++ FCiencias/FCiencias/FCienciasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FCienciasApp.swift
+//  FCiencias
+//
+//  Created by Luis Augusto Silva on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FCienciasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FCiencias/FCienciasTests/FCienciasTests.swift
@@ -0,0 +1,17 @@
+//
+//  FCienciasTests.swift
+//  FCienciasTests
+//
+//  Created by Luis Augusto Silva on 4/10/24.
+//
+
+import Testing
+@testable import FCiencias
+
+struct FCienciasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
